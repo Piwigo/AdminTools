0857b7221d9aa959ede9de76a7d6af107a775faa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-06 06:44:35 +0100

    Add th_TH, thanks to pradit

---
+++ language/th_TH/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Closed icon position'] = 'ตำแหน่งไอคอนปิด';
+$lang['Debug languages'] = 'ตรวจแก้จุดบกพร่องภาษา';
+$lang['Debug template'] = 'ตรวจแก้จุดบกพร่องเทมเพลต';
+$lang['Properties page'] = 'คุณสมบัติของเพจ';
+$lang['Quick edit'] = 'แก้ไขอย่างรวดเร็ว';
+$lang['Revert'] = 'ย้อนกลับ';
+$lang['Save'] = 'บันทึก';
+$lang['Save visit in history'] = 'บันทึกการเยี่ยมชมในประวัติ';
+$lang['Saved'] = 'บันทึกแล้ว';
+$lang['Show SQL queries'] = 'แสดงการสืบค้น SQL';
+$lang['View as'] = 'ดูในแบบ';
+$lang['Viewing as <b>%s</b>.'] = 'กำลังดูในแบบ <b>%s</b>.';
+$lang['left'] = 'ซ้าย';
+$lang['right'] = 'ขวา';
