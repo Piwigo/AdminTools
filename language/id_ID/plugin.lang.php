2646ed535aae752ee43a86f46645319906a0c9ab
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-05-04 06:36:57 +0200

    Add id_ID, thanks to tsaliscepu

---
+++ language/id_ID/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Combine JS&CSS'] = 'Gabungkan JS&amp;CSS';
+$lang['Give access to quick edit to photo owners even if they are not admin'] = 'Berikan akses ke edit cepat ke pemilik foto meskipun mereka bukan admin';
+$lang['Open toolbar by default'] = 'Buka toolbar secara default';
+$lang['Properties page'] = 'Properti halaman';
+$lang['Quick edit'] = 'Edit cepat';
+$lang['Revert'] = 'Kembalikan';
+$lang['Save'] = 'Simpan';
+$lang['Saved'] = 'Tersimpan';
+$lang['View as'] = 'Tampilkan sebagai';
+$lang['Viewing as <b>%s</b>.'] = 'Menampilkan sebagai <b>%s</b>.';
+$lang['left'] = 'kiri';
+$lang['right'] = 'kanan';
