ca02f06756087f79984fd59e1e6460c518073e82
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-05-01 10:48:35 +0200

    Add eu_ES, thanks to bixerdo

---
+++ language/eu_ES/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['right'] = 'eskuma';
+$lang['left'] = 'ezkerra';
+$lang['Viewing as <b>%s</b>.'] = '<b>%s</b> bezala ikusten.';
+$lang['View as'] = 'Ikusi honela';
+$lang['Show SQL queries'] = 'Erakutsi SQL bilaketak';
+$lang['Save visit in history'] = 'Gorde bisita historialan';
+$lang['Saved'] = 'Gordeta';
+$lang['Save'] = 'Gorde';
+$lang['Revert'] = 'Itzuli';
+$lang['Quick edit'] = 'Edizio azkarra';
+$lang['Properties page'] = 'Propietateak orria';
+$lang['Open toolbar by default'] = 'Ireki tresna-barra modu lehenetsian';
+$lang['Give access to quick edit to photo owners even if they are not admin'] = 'Eman azkar editatzeko aukera argazkien jabeei administrariak ez badira ere';
+$lang['Debug template'] = 'Araztu txantiloia';
+$lang['Debug languages'] = 'Araztu hizkuntzak';
+$lang['Combine JS&CSS'] = 'Konbinatu JS eta CSS';
+$lang['Closed icon position'] = 'Ikonoen posizio finkoa';
