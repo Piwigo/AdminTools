7e8129264222a0cc76cc18dbc18d0e6d06ab7056
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-05-30 06:20:44 +0200

    Add en_GB, thanks to Pat2309

---
+++ language/en_GB/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Closed icon position'] = 'Closed icon position';
+$lang['Combine JS&CSS'] = 'Combine JS&amp;CSS';
+$lang['Debug languages'] = 'Debug languages';
+$lang['Debug template'] = 'Debug template';
+$lang['Give access to quick edit to photo owners even if they are not admin'] = 'Give access to quick edit to photo owners even if they are not admin';
+$lang['Open toolbar by default'] = 'Open toolbar by default';
+$lang['Properties page'] = 'Properties page';
+$lang['Quick edit'] = 'Quick edit';
+$lang['Revert'] = 'Revert';
+$lang['Save'] = 'Save';
+$lang['Save visit in history'] = 'Save visit in history';
+$lang['Saved'] = 'Saved';
+$lang['Show SQL queries'] = 'Show SQL queries';
+$lang['View as'] = 'View as';
+$lang['Viewing as <b>%s</b>.'] = 'Viewing as <b>%s</b>.';
+$lang['left'] = 'left';
+$lang['right'] = 'right';
