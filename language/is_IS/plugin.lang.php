e4058ba339b57c2e79413af2965b87ba8c255710
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-08 20:50:42 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Closed icon position'] = 'Lokuð táknstaða';
+$lang['Combine JS&CSS'] = 'Sameina JS&amp;CSS';
+$lang['Debug languages'] = 'Villuleita tungumál';
+$lang['Debug template'] = 'Villuleita sniðmát';
+$lang['Give access to quick edit to photo owners even if they are not admin'] = 'Veittu eigendum mynda aðgang að skyndibreytingum, jafnvel þótt þeir séu ekki stjórnandi';
+$lang['Open toolbar by default'] = 'Opna tækjastikuna sjálfgefið';
+$lang['Properties page'] = 'Eigindasíða';
+$lang['Quick edit'] = 'Skyndibreyting';
+$lang['Revert'] = 'Fara tilbaka';
+$lang['Save'] = 'Vista';
+$lang['Save visit in history'] = 'Vista heimsókn í sögu';
+$lang['Saved'] = 'Vistað';
+$lang['Show SQL queries'] = 'Sýna SQL fyrirspurnir';
+$lang['View as'] = 'Skoða sem';
+$lang['Viewing as <b>%s</b>.'] = 'Skoða sem <b>%s</b>.';
+$lang['left'] = 'vinstri';
+$lang['right'] = 'hægri';
