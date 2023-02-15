ff330d91afb799e0494acc84c54354e5ff9b05a8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-02-15 20:41:04 +0100

    Update es_MX, thanks to grantiago

---
+++ language/es_MX/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Closed icon position'] = 'Posición del icono cerrado';
+$lang['Combine JS&CSS'] = 'Combinar JS y CSS';
+$lang['Debug languages'] = 'Debug idiomas';
+$lang['Debug template'] = 'Debug plantilla';
+$lang['Give access to quick edit to photo owners even if they are not admin'] = 'Otorgue acceso a la edición rápida a los propietarios de fotos, incluso si no son administradores';
+$lang['Open toolbar by default'] = 'Abrir barra de herramientas por defecto';
+$lang['Properties page'] = 'Página de propiedades';
+$lang['Quick edit'] = 'Edición rapida';
+$lang['Revert'] = 'Volver';
+$lang['Save'] = 'Guadar';
+$lang['Save visit in history'] = 'Guardar visita en el historial';
+$lang['Saved'] = 'Guardado';
+$lang['Show SQL queries'] = 'Mostrar consultas SQL';
+$lang['View as'] = 'Visto como';
+$lang['Viewing as <b>%s</b>.'] = 'Viendo como <b>%s</b>.';
+$lang['left'] = 'Izquierda';
+$lang['right'] = 'Derecha';
