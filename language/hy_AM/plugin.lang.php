ad5622bcb662e0e49818b8bb5402acbf78a7c32a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-01-19 15:02:56 +0100

    Update hy_AM, thanks to gayane

---
+++ language/hy_AM/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Closed icon position'] = '"Փակել" պատկերակի դիրքը';
+$lang['Combine JS&CSS'] = 'Միավորել JS&amp;CSS';
+$lang['Debug languages'] = 'Վրիպազերծման լեզուներ';
+$lang['Debug template'] = 'Վրիպազերծման ձևանմուշ';
+$lang['Give access to quick edit to photo owners even if they are not admin'] = 'Արագ խմբագրման թույլտվություն տալ լուսանկարների սեփականատերերին, նույնիսկ եթե նրանք ադմինիստրատոր չեն';
+$lang['Open toolbar by default'] = 'Բացել գործիքագոտին default-ով';
+$lang['Properties page'] = 'Հատկությունների էջ';
+$lang['Quick edit'] = 'Արագ խմբագրություն';
+$lang['Revert'] = 'Վերադարձ';
+$lang['Save'] = 'Պահպանել';
+$lang['Save visit in history'] = 'Պահպանել այցը պատմության մեջ';
+$lang['Saved'] = 'Պահպանվեց';
+$lang['Show SQL queries'] = 'Ցույց տալ SQL հարցումները';
+$lang['View as'] = 'Դիտել որպես';
+$lang['Viewing as <b>%s</b>.'] = 'Դիտվում է որպես <b>%s</b>.';
+$lang['left'] = 'ձախ';
+$lang['right'] = 'աջ';
